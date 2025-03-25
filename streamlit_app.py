fef758cfc11e2db5bf5f16d88ba9ad96b61111c2
jaypbazar <jabazar@my.cspc.edu.ph>
2025-03-25 15:04:29 +0800

    commit 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
 
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
